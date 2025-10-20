4dc798185fd59f23b8d61e872e68f766ffcfe2df
Kacper Zieliński <kzielinski@omenproductions.com>
2025-10-20 19:34:08 +0200

    fix/wrong android:name directive in manifest

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,8 +30,6 @@ android {
 
     defaultConfig {
         applicationId = "com.srzak.zakstreamer"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
