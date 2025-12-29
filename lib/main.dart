90da9c29a47fdc81507bad45aa05ced767b0635d
adonizy <adonizy11@gmail.com>
2025-12-29 18:16:07 +0100

    chore: Add logging setup on app start

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:audio_session/audio_session.dart';
 import "package:logging/logging.dart";
+import 'package:logging/logging.dart';
 import 'package:simple_animations/animation_builder/custom_animation_builder.dart';
 import 'page_manager.dart';
 import 'service_locator.dart';
@@ -9,6 +10,10 @@ import 'package:flutter/services.dart';
 Future<void> main() async {
   final log = Logger('Main');
   Logger.root.level = Level.ALL; // defaults to Level.INFO
+void main() async {
+  // Setup logging
+  final log = Logger('Init');
+  Logger.root.level = Level.ALL;
   Logger.root.onRecord.listen((record) {
     debugPrint('${record.level.name}: ${record.time}: ${record.message}');
   });
