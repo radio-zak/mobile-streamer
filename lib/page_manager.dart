0ac97210dfb672964507aa4d61076d769d25fdfc
adonizy <adonizy11@gmail.com>
2025-12-29 18:06:57 +0100

    refactor: Add PageManager for UI state management

--- lib/page_manager.dart
+++ lib/page_manager.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/foundation.dart';
 import 'package:audio_service/audio_service.dart';
 import 'service_locator.dart';
+import 'schedule_service.dart' as schedule_service;
 
-enum ButtonState { playing, paused, loading }
+enum ButtonState { playing, paused, loading, error }
 
 class PlayButtonNotifier extends ValueNotifier<ButtonState> {
   PlayButtonNotifier() : super(_initialValue);
@@ -10,13 +11,20 @@ class PlayButtonNotifier extends ValueNotifier<ButtonState> {
 }
 
 class PageManager {
+  final log = Logger('PageManager');
+
+  // --- Notifiers for UI ---
   final playButtonNotifier = PlayButtonNotifier();
   void init() async {
     await _listenToPlaybackState();
   }
 
+  // --- Audio Handling ---
   final _audioHandler = getIt<AudioHandler>();
   Future<void> _listenToPlaybackState() async {
+  void play() => _audioHandler.play();
+  void pause() => _audioHandler.pause();
+  void _listenToPlaybackState() {
     _audioHandler.playbackState.listen((playbackState) {
       final isPlaying = playbackState.playing;
       final processingState = playbackState.processingState;
@@ -26,6 +34,9 @@ class PageManager {
       } else if (!isPlaying) {
         playButtonNotifier.value = ButtonState.paused;
       } else if (processingState != AudioProcessingState.completed) {
+      } else if (processingState == AudioProcessingState.error) {
+        playButtonNotifier.value = ButtonState.error;
+        showDisconnectionNotification();
         playButtonNotifier.value = ButtonState.playing;
       } else {
         _audioHandler.pause();
