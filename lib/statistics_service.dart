f12b1f60f6b1114636d0f9df782895209842fdf5
adonizy <adonizy11@gmail.com>
2026-01-13 15:26:09 +0100

    feat: Implement total listening time tracking

--- lib/statistics_service.dart
+++ lib/statistics_service.dart
@@ -1,11 +1,47 @@
+import 'dart:async';
+
+import 'package:audio_service/audio_service.dart';
+import 'package:flutter/foundation.dart';
 import 'statistics_repository.dart';
 
 class StatisticsService {
   final StatisticsRepository _repository;
+  final AudioHandler _audioHandler;
+  Timer? _timer;
+  int _totalListeningTime = 0;
 
-  StatisticsService(this._repository);
+  StatisticsService(this._repository, this._audioHandler);
 
   Future<void> init() async {
     await _repository.saveInstallDate();
+    _totalListeningTime = await _repository.getTotalListeningTime();
+
+    _audioHandler.playbackState.listen((playbackState) {
+      final isPlaying = playbackState.playing;
+      if (isPlaying) {
+        _startTimer();
+      } else {
+        _stopTimer();
+      }
+    });
+  }
+
+  void _startTimer() {
+    _timer ??= Timer.periodic(const Duration(seconds: 1), (_) {
+      _totalListeningTime++;
+      if (_totalListeningTime % 10 == 0) {
+        _repository.saveTotalListeningTime(_totalListeningTime);
+      }
+    });
+  }
+
+  void _stopTimer() {
+    _timer?.cancel();
+    _timer = null;
+  }
+
+  void dispose() {
+    _stopTimer();
+    _repository.saveTotalListeningTime(_totalListeningTime);
   }
 }
