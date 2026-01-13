a0edee617249ffaa96f7748eb0e3219b7de356c5
adonizy <adonizy11@gmail.com>
2026-01-13 15:22:39 +0100

    feat: Extend statistics repository with total listening time

--- lib/statistics_repository.dart
+++ lib/statistics_repository.dart
@@ -2,6 +2,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class StatisticsRepository {
   static const _installDateKey = 'install_date';
+  static const _totalListeningTimeKey = 'total_listening_time';
 
   Future<void> saveInstallDate() async {
     final prefs = await SharedPreferences.getInstance();
@@ -14,5 +15,15 @@ class StatisticsRepository {
     final prefs = await SharedPreferences.getInstance();
     final dateString = prefs.getString(_installDateKey);
     return dateString != null ? DateTime.parse(dateString) : null;
+  }
+
+  Future<void> saveTotalListeningTime(int seconds) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt(_totalListeningTimeKey, seconds);
+  }
+
+  Future<int> getTotalListeningTime() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getInt(_totalListeningTimeKey) ?? 0;
   }
 }
