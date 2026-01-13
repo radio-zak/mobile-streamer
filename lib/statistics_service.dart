755fc86050bc0cc9b889509bf661b0ae70cd1dac
adonizy <adonizy11@gmail.com>
2026-01-13 16:02:41 +0100

    feat: Implement session tracking logic

--- lib/statistics_service.dart
+++ lib/statistics_service.dart
@@ -7,8 +7,10 @@ import 'statistics_repository.dart';
 class StatisticsService {
   final StatisticsRepository _repository;
   final AudioHandler _audioHandler;
-  Timer? _timer;
+  Timer? _totalTimeTimer;
+  Timer? _sessionTimer;
   int _totalListeningTime = 0;
+  int _currentSessionTime = 0;
 
   StatisticsService(this._repository, this._audioHandler);
 
@@ -19,9 +21,9 @@ class StatisticsService {
     _audioHandler.playbackState.listen((playbackState) {
       final isPlaying = playbackState.playing;
       if (isPlaying) {
-        _startTimer();
+        _startTimers();
       } else {
-        _stopTimer();
+        _stopTimers();
       }
     });
   }
@@ -34,22 +36,51 @@ class StatisticsService {
     return _repository.getTotalListeningTime();
   }
 
-  void _startTimer() {
-    _timer ??= Timer.periodic(const Duration(seconds: 1), (_) {
+  Future<int> getLongestSession() {
+    return _repository.getLongestSession();
+  }
+
+  Future<int> getShortestSession() {
+    return _repository.getShortestSession();
+  }
+
+  void _startTimers() {
+    _totalTimeTimer ??= Timer.periodic(const Duration(seconds: 1), (_) {
       _totalListeningTime++;
       if (_totalListeningTime % 10 == 0) {
         _repository.saveTotalListeningTime(_totalListeningTime);
       }
     });
+    _sessionTimer ??= Timer.periodic(const Duration(seconds: 1), (_) {
+      _currentSessionTime++;
+    });
   }
 
-  void _stopTimer() {
-    _timer?.cancel();
-    _timer = null;
+  void _stopTimers() {
+    _totalTimeTimer?.cancel();
+    _totalTimeTimer = null;
+    _sessionTimer?.cancel();
+    _sessionTimer = null;
+    _updateSessionStats();
+    _currentSessionTime = 0;
+  }
+
+  Future<void> _updateSessionStats() async {
+    if (_currentSessionTime == 0) return;
+
+    final longestSession = await _repository.getLongestSession();
+    if (_currentSessionTime > longestSession) {
+      await _repository.saveLongestSession(_currentSessionTime);
+    }
+
+    final shortestSession = await _repository.getShortestSession();
+    if (_currentSessionTime < shortestSession || shortestSession == 0) {
+      await _repository.saveShortestSession(_currentSessionTime);
+    }
   }
 
   void dispose() {
-    _stopTimer();
+    _stopTimers();
     _repository.saveTotalListeningTime(_totalListeningTime);
   }
 }
