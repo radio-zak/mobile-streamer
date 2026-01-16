6c12a792706facee6f56e345372ce7c1ed85e6fe
adonizy <adonizy11@gmail.com>
2026-01-16 11:52:51 +0100

    feat: Integrate existing pages into bottom navigation

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,5 +1,8 @@
 import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:zakstreamer/home_page.dart';
+import 'package:zakstreamer/schedule_page.dart';
+import 'package:zakstreamer/statistics_page.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -11,11 +14,10 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   int _bottomNavIndex = 0;
 
-  // TODO: Replace with actual pages
   final List<Widget> _pages = [
-    const Center(child: Text('Strona Główna')),
-    const Center(child: Text('Ramówka')),
-    const Center(child: Text('Statystyki')),
+    const HomePage(),
+    const SchedulePage(),
+    const StatisticsPage(),
     const Center(child: Text('Więcej')),
   ];
 
@@ -38,7 +40,9 @@ class _MainScreenState extends State<MainScreen> {
         leftCornerRadius: 32,
         rightCornerRadius: 32,
         onTap: (index) => setState(() => _bottomNavIndex = index),
-        //TODO: Add other properties to style the bar
+        backgroundColor: Colors.black,
+        activeColor: Colors.white,
+        inactiveColor: Colors.grey,
       ),
     );
   }
