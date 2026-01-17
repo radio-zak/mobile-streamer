e88b2087df355dd14f787f41ef34ce69862b3afc
adonizy <adonizy11@gmail.com>
2026-01-17 12:29:14 +0100

    feat: Redesign navigation with a centered FAB

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,5 +1,6 @@
 import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:zakstreamer/contact_page.dart';
 import 'package:zakstreamer/home_page.dart';
 import 'package:zakstreamer/schedule_page.dart';
 import 'package:zakstreamer/statistics_page.dart';
@@ -12,13 +13,13 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  int _bottomNavIndex = 0;
+  int _pageIndex = 2; // Start on Home
   late PageController _pageController;
 
   @override
   void initState() {
     super.initState();
-    _pageController = PageController();
+    _pageController = PageController(initialPage: _pageIndex);
   }
 
   @override
@@ -28,17 +29,19 @@ class _MainScreenState extends State<MainScreen> {
   }
 
   final List<Widget> _pages = [
-    const HomePage(),
     const SchedulePage(),
     const StatisticsPage(),
-    const Center(child: Text('Więcej')),
+    const HomePage(),
+    const ContactPage(),
+    const Center(child: Text('Tutaj pojawią się nowe podstrony, standby ')),
   ];
 
-  final iconList = <IconData>[
-    Icons.home,
-    Icons.calendar_today,
-    Icons.bar_chart,
-    Icons.more_horiz,
+  final List<IconData> _icons = [
+    Icons.calendar_today_outlined,
+    Icons.bar_chart_outlined,
+    Icons.headphones_rounded,
+    Icons.phone_outlined,
+    Icons.more_horiz_outlined,
   ];
 
   @override
@@ -48,26 +51,46 @@ class _MainScreenState extends State<MainScreen> {
         controller: _pageController,
         children: _pages,
         onPageChanged: (index) {
-          setState(() => _bottomNavIndex = index);
+          setState(() => _pageIndex = index);
         },
       ),
-      bottomNavigationBar: AnimatedBottomNavigationBar(
-        icons: iconList,
-        activeIndex: _bottomNavIndex,
+      bottomNavigationBar: AnimatedBottomNavigationBar.builder(
+        itemCount: _icons.length,
+        tabBuilder: (int index, bool isActive) {
+          final color = isActive ? Colors.tealAccent : Colors.grey;
+          // Middle button style
+          if (index == 2) {
+            return Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: isActive ? Colors.tealAccent : Colors.grey[850],
+              ),
+              padding: const EdgeInsets.all(12),
+              child: Icon(
+                _icons[index],
+                color: isActive ? Colors.black : Colors.white,
+                size: 28,
+              ),
+            );
+          }
+          // Regular button style
+          return Icon(
+            _icons[index],
+            size: 24,
+            color: color,
+          );
+        },
+        activeIndex: _pageIndex,
         gapLocation: GapLocation.none,
         notchSmoothness: NotchSmoothness.verySmoothEdge,
         leftCornerRadius: 32,
         rightCornerRadius: 32,
-        onTap: (index) {
-          _pageController.animateToPage(
-            index,
-            duration: const Duration(milliseconds: 300),
-            curve: Curves.ease,
-          );
-        },
+        onTap: (index) => _pageController.animateToPage(
+          index,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.ease,
+        ),
         backgroundColor: Colors.black,
-        activeColor: Colors.tealAccent,
-        inactiveColor: Colors.grey,
       ),
     );
   }
