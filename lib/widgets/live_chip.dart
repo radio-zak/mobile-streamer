159679f01677304bbb4ae5e4a83b10d7752afc08
Kacper Zieliński <kzielinski@omenproductions.com>
2026-01-20 22:56:16 +0100

    fix: LiveChip not having a key parameter

--- lib/widgets/live_chip.dart
+++ lib/widgets/live_chip.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class LiveChip extends StatelessWidget {
+  const LiveChip({super.key});
   @override
   Widget build(BuildContext context) {
     return Container(
