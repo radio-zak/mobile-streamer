a4feff1bd167f0d5af949805d315871e27284d16
adonizy <adonizy11@gmail.com>
2026-01-27 15:14:20 +0100

    test(streamer): Verify initial state and audio source

--- test/streamer_test.dart
+++ test/streamer_test.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:mockito/annotations.dart';
@@ -14,11 +16,33 @@ void main() {
 
     setUp(() {
       mockAudioPlayer = MockAudioPlayer();
+      // Mock the stream controllers
+      when(mockAudioPlayer.playbackStateStream).thenAnswer((_) => Stream.value(PlaybackState(true, ProcessingState.idle)));
+      when(mockAudioPlayer.processingStateStream).thenAnswer((_) => Stream.value(ProcessingState.idle));
+      when(mockAudioPlayer.playingStream).thenAnswer((_) => Stream.value(false));
+      when(mockAudioPlayer.durationStream).thenAnswer((_) => Stream.value(Duration.zero));
+      when(mockAudioPlayer.positionStream).thenAnswer((_) => Stream.value(Duration.zero));
+      when(mockAudioPlayer.bufferedPositionStream).thenAnswer((_) => Stream.value(Duration.zero));
+      when(mockAudioPlayer.sequenceStateStream).thenAnswer((_) => Stream.value(null));
+      when(mockAudioPlayer.sequenceStream).thenAnswer((_) => Stream.value(null));
+      when(mockAudioPlayer.icyMetadataStream).thenAnswer((_) => Stream.value(null));
+
+      // Mock the setAudioSource method to complete successfully
+      when(mockAudioPlayer.setAudioSource(any)).thenAnswer((_) async => Duration.zero);
+
       streamer = Streamer(audioPlayer: mockAudioPlayer);
     });
 
-    test('initial state is correct', () {
-      // Test logic will go here
+    test('correctly sets audio source on initialization', () async {
+      // The Streamer's constructor is called in setUp.
+      // We need to wait for the asynchronous operations within the constructor to complete.
+      await streamer.init();
+
+      // Verify that setAudioSource was called with the correct URL.
+      final verificationResult = verify(mockAudioPlayer.setAudioSource(captureAny));
+      final captured = verificationResult.captured.single as LockCachingAudioSource;
+
+      expect(captured.uri.toString(), equals('https://stream.radiozak.pl:8443/zak.aac'));
     });
   });
 }
