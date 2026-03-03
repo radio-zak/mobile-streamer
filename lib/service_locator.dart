a305fff137637f181339d09d589494e03ab699bf
adonizy <adonizy11@gmail.com>
2026-03-03 23:32:31 +0100

    feat: Register FavoritesService in service_locator

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -4,6 +4,7 @@ import 'streamer.dart';
 import 'package:get_it/get_it.dart';
 import 'schedule_service.dart';
 import 'now_playing.dart';
+import 'favorites_service.dart';
 
 GetIt getIt = GetIt.instance;
 
@@ -12,4 +13,9 @@ Future<void> setupServiceLocator() async {
   getIt.registerLazySingleton<PageManager>(() => PageManager());
   getIt.registerLazySingleton<NowPlaying>(() => NowPlaying());
   getIt.registerLazySingleton<ScheduleService>(() => ScheduleService());
+
+  // Initialize FavoritesService
+  final favoritesService = FavoritesService();
+  await favoritesService.init();
+  getIt.registerSingleton<FavoritesService>(favoritesService);
 }
